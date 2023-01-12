470fa2609946548adbcc6b2b56057508f66f1ce1
bcova <brandoncova@gmail.com>
2023-01-12 14:04:57 -0600

    fixed seed

--- db/index.js
+++ db/index.js
@@ -7,7 +7,7 @@
 const { Client } = require('pg'); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client('postgres://localhost:5432/users');
 
 module.exports = {
   client,
